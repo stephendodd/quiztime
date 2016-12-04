da62fcc80f15448204fffed2df6c3f8dfc3cb69a
Stephen Dodd <stephen.dodd@rea-group.com>
2016-12-04 17:22:14 +1100

    Add dynamic quiz api to display multiple quizzes

--- app.rb
+++ app.rb
@@ -2,18 +2,11 @@ require 'sinatra'
 require 'json'
 require 'sqlite3'
 
-questionArray = []
 userArray = []
 
 begin
 
   db = SQLite3::Database.open "quizzes.sqlite"
-  stm = db.prepare "SELECT * FROM quiz1"
-
-  rs = stm.execute
-  rs.each do |question|
-    questionArray.push(question)
-  end
 
   users = db.prepare "SELECT * FROM users WHERE quiz == 1"
   usersResult = users.execute
@@ -27,20 +20,12 @@ rescue SQLite3::Exception => e
   puts e
 
 ensure
-  stm.close if stm
   users.close if users
   db.close if db
 end
 
 get '/' do
   File.new('public/index.html').read
-end
-
-get '/quiz1' do
-
-  content_type :json
-  {quiz: questionArray}.to_json
-
 end
 
 get '/1/scores' do
@@ -50,5 +35,33 @@ get '/1/scores' do
   end
   content_type :json
   {scores: userArray}.to_json
+
+end
+
+get '/quiz/:quizNo' do
+  questionArray = []
+  quizString = "quiz#{params[:quizNo]}"
+
+  begin
+
+    db2 = SQLite3::Database.open "quizzes.sqlite"
+    stm = db2.prepare "SELECT * FROM #{quizString}"
+
+    rs = stm.execute
+    rs.each do |question|
+      questionArray.push(question)
+    end
+
+  rescue SQLite3::Exception => e
+    puts "Error occured"
+    puts e
+
+  ensure
+    stm.close if stm
+    db2.close if db2
+  end
+
+  content_type :json
+  {quiz: questionArray}.to_json
 
 end
